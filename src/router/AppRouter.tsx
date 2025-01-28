cf46d99947e940800f8c57805b9649f0d5820c43
fer <fer@fers-iMac.local>
2025-01-27 18:19:31 -0700

    koko

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -52,10 +52,10 @@ const AppRouter = () => {
                 <Route path="/auth/register" element={<SignUp startRegister={startRegister} status={status} />} />*/}
 
                 <Route path="/categorias" element={<Categorias />} />
-                <Route path="/tov/users"  element={<Users  />} />
+                <Route path="/tov"  element={<Users  />} />
                 <Route path="/productos"  element={<Productos  />} />
 
-                <Route path="/*" element={<Navigate to="/tov/users" />} /> 
+                <Route path="/*" element={<Navigate to="/tov" />} /> 
             </Routes>
         </div>
     )
