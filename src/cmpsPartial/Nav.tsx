f481dd339f5f70384249b820f44da5e1ce2cb93d
fer <fer@fers-iMac.local>
2025-01-27 18:12:15 -0700

    kok

--- src/cmpsPartial/Nav.tsx
+++ src/cmpsPartial/Nav.tsx
@@ -18,14 +18,14 @@ export const Nav = ({ startLogout, status, user }) => {
     {/*  { localStorage.status == 'authenticated' &&  */}
           <div>
             <span>
-              <Link style={{color:"white"}} to="/tov/users"  className='mx-5'>Users     </Link>
+              <Link style={{color:"white"}} to="/users"      className='mx-5'>Users     </Link>
               <Link style={{color:"white"}} to="/productos"  className='mx-5'>Productos </Link>
               <Link style={{color:"white"}} to="/categorias" className='mx-5'>Categorias</Link>
             </span>
 
         {/*    <Link className="btn btn-outline-danger" to="/auth/login" onClick={ startLogout }>
               <i className="fas fa-sign-out-alt"></i>salir
-            </Link>
+            </Link>*/}
           </div>
 
     {/*     } */}
